a254ce54c44187d6b6ea6ebb1b6a88e6bdee3347
Javed Khan <73348500+javedkhan0@users.noreply.github.com>
2022-06-03 18:58:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAVXN2TQEOKI5RUH2M"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
